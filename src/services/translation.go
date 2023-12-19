44749be300d9fc5f2ced232ff2852b9979791416
Psycomentis06 <alibenamor30@gmail.com>
2023-12-19 16:25:57 +0100

    Translation setup

---
+++ src/services/translation.go
@@ -0,0 +1,18 @@
+package services
+
+type Translation struct {
+}
+
+func GetAvailableLocales() {}
+
+func SetDefaultLocale(locale string) {}
+
+func SetLocale(locale string) {}
+
+func GetAdminLocale() {}
+
+func GetAdminLocaleFile() {}
+
+func GetAppLocale() {}
+
+func GetAppLocaleFile() {}
